40bad766d304edd864e7f9467bd936a6ee3566da
Mia Wenjin Zhang <wenjinz@andrew.cmu.edu>
2019-10-29 14:11:01 -0400

    search bar get song list

--- src/mysite/synphony/views.py
+++ src/mysite/synphony/views.py
@@ -2,4 +2,16 @@ from django.shortcuts import render
 
 
 def index(request):
+    if request.method == 'POST' and 'get-list' in request.POST:
+        return displaySongList(request)
     return render(request, 'synphony/index.html', {})
+
+
+def displaySongList(request):
+    title = request.POST.get('get-list')
+    # use song title to call api
+    # get json
+    # process json -> dic listof Songs
+    # id, name, author - list
+    # render page with var list
+    pass
