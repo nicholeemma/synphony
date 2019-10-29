bdacc4c22194178c571aa32d0e9bfe52ef306f25
Mia Wenjin Zhang <wenjinz@andrew.cmu.edu>
2019-10-29 12:56:17 -0400

    add studio in participant

--- specification/models.py
+++ specification/models.py
@@ -10,16 +10,17 @@ class Music(models.Model):
     name = models.CharField(max_length=30)
     url = models.CharField(max_length=255)
     description = models.CharField(max_length=255, blank=True)
-    lyrics = models.TextField(max_length=255, blank=True) ##
-    liked_user = models.ManyToManyField(User,on_delete=models.CASCADE)
+    lyrics = models.TextField(max_length=255, blank=True)
+    liked_user = models.ManyToManyField(User, on_delete=models.CASCADE)
 
 # The goal is to record the liked music of a certain user
 # We should implement abstract user, liked_music should be an attribute of it, as we don't know to do that
-# For now, we just extend the user here  
-class Syner(models.Model):
-    user = models.OneToOneField(User,on_delete=models.CASCADE) 
-    liked_music = models.ManyToManyField(Music,on_delete=models.CASCADE)
+# For now, we just extend the user here
 
+
+class Syner(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    liked_music = models.ManyToManyField(Music, on_delete=models.CASCADE)
 
 
 # class Playlist(models.Model):
@@ -36,33 +37,35 @@ class Syner(models.Model):
 
 class Participant(models.Model):
 
-    participant_user = models.ForeignKey(User,on_delete=models.CASCADE)
-    role = models.CharField(max_length=10,default='participant')
+    participant_user = models.ForeignKey(User, on_delete=models.CASCADE)
+    role = models.CharField(max_length=10, default='participant')
+    studio = models.ForeignKey(Studio, on_delete=models.CASCADE)
+
 
 class Studio(models.Model):
 
     name = models.CharField(max_length=30)
-    participant = models.ManyToManyField(Participant)
-    music = models.ManyToManyField(Music,on_delete=models.CASCADE)
+    # participants = models.ManyToManyField(Participant)
+    music = models.ManyToManyField(Music, on_delete=models.CASCADE)
     # record whether the studio is active or not
     status = models.BooleanField(default=True)
     # constraint will be set in form.py cannot be larger than 10
     headcount = models.IntegerField(default=10)
     # field_for_sharablelink
-    host = models.ForeignKey(User,on_delete=models.CASCADE)
+    host = models.ForeignKey(User, on_delete=models.CASCADE)
     start_time = models.DateTimeField(auto_now_add=True)
     end_time = models.DateTimeField(blank=True)
 
+
 class Comment(models.Model):
 
-    user_name = models.ForeignKey(User,on_delete=models.CASCADE)
+    user_name = models.ForeignKey(User, on_delete=models.CASCADE)
     text = models.CharField(max_length=300)
     created_on = models.DateTimeField(auto_now_add=True)
-    commented_on = models.ForeignKey(Studio,on_delete=models.CASCADE)
+    commented_on = models.ForeignKey(Studio, on_delete=models.CASCADE)
+
 
 class History(models.Model):
 
-    user = models.OneToOneField(User,on_delete=models.CASCADE)   
-    studio = models.ForeignKey(Studio,on_delete=models.CASCADE)
-
-
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    studio = models.ForeignKey(Studio, on_delete=models.CASCADE)
