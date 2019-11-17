0e05047b7016d2e6edf35368ca4bbcad08cc0ca6
mylibrar <suqi_sun@outlook.com>
2019-11-17 09:07:51 -0500

    Add websocket.

--- src/mysite/mysite/routing.py
+++ src/mysite/mysite/routing.py
@@ -1,6 +1,11 @@
-# mysite/routing.py
-from channels.routing import ProtocolTypeRouter
+
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+import synphony.routing
 
 application = ProtocolTypeRouter({
     # (http->django views is added by default)
+    'websocket': AuthMiddlewareStack(
+       URLRouter( synphony.routing.websocket_urlpatterns )
+    ),
 })
--- src/mysite/mysite/settings.py
+++ src/mysite/mysite/settings.py
@@ -74,7 +74,14 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'mysite.wsgi.application'
 ASGI_APPLICATION = "mysite.routing.application"  # Django Channel
-
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            "hosts": [('127.0.0.1', 6379)],
+        },
+    },
+}
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
