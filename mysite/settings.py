e8ed8f0d892c91721e4866e7424a3664f2faa855
mylibrar <suqi_sun@outlook.com>
2019-12-02 17:15:42 -0500

    Update setting

--- mysite/settings.py
+++ mysite/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
-import os
+import os, django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -151,5 +151,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 # Configure Django App for Heroku.
-import django_heroku
+
 django_heroku.settings(locals())
