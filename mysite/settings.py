80ab4bf8bebfb36fa478e22679c020095fc3f0e9
mylibrar <suqi_sun@outlook.com>
2019-12-05 18:34:51 -0500

    Update settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -28,7 +28,7 @@ LOGOUT_REDIRECT_URL = '/'
 SECRET_KEY = 'm*s3k&i$b&4p*9gr*mbm$(v03=k%n1t2ko#18c=r439w&o=r1q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["0.0.0.0", "127.0.0.1", "localhost"]
 
